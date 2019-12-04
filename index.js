e8c2ca2c7ee759fca1cb08b68cb32d673f291626
lesley <lesleybanadzem2017@gmail.com>
2019-12-04 12:17:05 -0800

    Completed endpoint for  Getting data for a specific user

--- index.js
+++ index.js
@@ -15,9 +15,48 @@ app.get('/api/users', (req, res)=>{
                 data: response    
             })
     }).catch(err=>{
-        console.log(err)
+        res.status(404).send({
+            message: 'Not found',
+            data: err    
+        })
     })
   
 })
+app.get('/api/users/:id', (req, res)=>{
+    const id = req.params.id;
+    if(!id){
+        res.status(404).json({ message: 
+            "The user with the specified ID does not exist." })
+    } else {
+       
+        db.findById(id).then(response =>{
+            res.status(200).send({
+                    message: 'Request successful',
+                    data: response    
+                })
 
+        }).catch(err=>{
+            res.status(500).send({ errorMessage: "The user information could not be retrieved." })
+        })
+    }
+    
+  
+})
+
+// app.post('/api/users', (req, res)=>{
+//     console.log(req 
+//         )
+//     // db.find().then(response =>{
+//     //     res.status(200).send({
+//     //             message: 'Request successful',
+//     //             data: response    
+//     //         })
+//     // }).catch(err=>{
+//     //     res.status(404).send({
+//     //         message: 'Not found',
+//     //         data: err    
+//     //     })
+//     // })
+//   res.status(200).send(req.body)
+// })
 app.listen(PORT, ()=> console.log(`Server listening to http://localhost:${PORT}`))
