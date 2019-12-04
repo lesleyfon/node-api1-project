ace2cbe8ed4aecb303aea3796a677c0f27ba5678
lesley <lesleybanadzem2017@gmail.com>
2019-12-04 12:53:32 -0800

    Wrote endpoint to Delete user

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.get('/api/users/:id', (req, res)=>{
   
 })
 
+//posting data
 app.post('/api/users', (req, res)=>{
     const {name, bio} = req.body;
     if(!name || !bio){
@@ -51,7 +52,7 @@ app.post('/api/users', (req, res)=>{
     } else{
         db.insert({name, bio})
             .then(response => {
-                console.log(response)
+
                 res.status(201).json({message: 'Post Successful', newUser: {name, bio}})
             })
             .catch(err => {
@@ -59,16 +60,26 @@ app.post('/api/users', (req, res)=>{
                 
             })
     }
-    // db.find().then(response =>{
-    //     res.status(200).send({
-    //             message: 'Request successful',
-    //             data: response    
-    //         })
-    // }).catch(err=>{
-    //     res.status(404).send({
-    //         message: 'Not found',
-    //         data: err    
-    //     })
-    // })
+   
+});
+
+//Delete User
+
+app.delete('/api/user/:id', (req, res)=>{
+    const id = req.params.id;
+    if(!id){
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else {
+       
+        db.remove(id).then(response =>{
+            console.log(response)
+            res.status(200).send({
+                    message: 'Successfully Deleted',  
+                })
+        }).catch(err=>{
+            res.status(500).send({ errorMessage: "The user could not be removed" })
+        })
+    }
+    
 })
 app.listen(PORT, ()=> console.log(`Server listening to http://localhost:${PORT}`))
