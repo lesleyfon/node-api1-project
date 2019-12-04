522d62cb2ae4dae348bd9e9de634eb568f1f079d
lesley <lesleybanadzem2017@gmail.com>
2019-12-03 16:11:00 -0800

    Wrote endpoint for getting users in then they git api/users

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db')
+const app = express();
+
+app.use(express.json())
+const PORT = 8080;
+
+app.get('/api/users', (req, res)=>{
+    db.find().then(response =>{
+        res.status(200).send({
+                message: 'Request successful',
+                data: response    
+            })
+    }).catch(err=>{
+        console.log(err)
+    })
+  
+})
+
+app.listen(PORT, ()=> console.log(`Server listening to http://localhost:${PORT}`))
